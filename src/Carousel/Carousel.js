c643cf372e0d8b7e0b0e7ec7578c4c5436483bbc
Jagan <djagadeesan42@gmail.com>
2025-07-14 22:55:46 +0530

    code change for css tech

---
+++ src/Carousel/Carousel.js
@@ -0,0 +1,111 @@
+import infrastructure from "../images/infrastructure.jpg";
+import revolving_door from "../images/revolving_door.jpg";
+import digital_door_Lock from "../images/digital_door_lock.jpg";
+import Electronic_lock from "../images/Electronic_locks.jpg";
+import glass_door from "../images/glass_door.jpg";
+
+function Carousel() {
+  return (
+    <div
+      id="carouselExampleRide"
+      className="carousel slide"
+      data-bs-ride="carousel"
+    >
+      <div className="carousel-indicators">
+        <button
+          type="button"
+          data-bs-target="#carouselExampleRide"
+          data-bs-slide-to="0"
+          className="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleRide"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleRide"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleRide"
+          data-bs-slide-to="3"
+          aria-label="Slide 3"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleRide"
+          data-bs-slide-to="4"
+          aria-label="Slide 3"
+        ></button>
+      </div>
+      <div className="carousel-inner">
+        <div className="carousel-item active" data-bs-interval="1000">
+          <img
+            src={infrastructure}
+            className="d-block w-100"
+            alt="infrastructure"
+          />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>First slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+          </div>
+        </div>
+        <div className="carousel-item" data-bs-interval="1000">
+          <img src={revolving_door} className="d-block w-100" alt="..." />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>First slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+          </div>
+        </div>
+        <div className="carousel-item" data-bs-interval="1000">
+          <img src={digital_door_Lock} className="d-block w-100" alt="..." />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>First slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+          </div>
+        </div>
+        <div className="carousel-item" data-bs-interval="1000">
+          <img src={Electronic_lock} className="d-block w-100" alt="..." />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>First slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+          </div>
+        </div>
+        <div className="carousel-item" data-bs-interval="1000">
+          <img src={glass_door} className="d-block w-100" alt="..." />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>First slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+          </div>
+        </div>
+      </div>
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleRide"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleRide"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
+  );
+}
+
+export default Carousel;
